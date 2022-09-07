484489d93dee118a634c02f85398eab1bfe11047
LeoLundqvist <leolundqvist05@gmail.com>
2022-09-07 14:06:56 +0200

    Converted input to int
    Gjorde en int som kallas num för att switchcasen ska fungera, la också till meny texten

--- Skola/src/SchoolSystem.cpp
+++ Skola/src/SchoolSystem.cpp
@@ -3,26 +3,41 @@
 void SchoolSystem::Run()
 {
 	int counter = 0;
+	int num = 0;
+
+	
+
+
+
+
 	while (true)
 	{
+		//Menyn
+		std::cout << "SchoolSystem\n";
+		std::cout << "1. Skapa ny skolklass\n";
+		std::cout << "2. Lägg till elev i klass\n";
+		std::cout << "3. Byt elevs klass\n";
+		std::cout << "4. Ta bort elev från klass\n";
+		std::cout << "5. Ta bort elev från skolsystemet\n";
+		std::cout << "6. Hitta information om elev\n";
+		std::cout << "7. Hitta information om klass\n";
+
+		//Input
 		std::string input = "";
 		std::getline(std::cin, input);
-		
-		 
-		switch(input)
+		num = stoi(input);
+
+		switch(num)
 		{
-			case "a":
+			case 1:
+				std::cout << "hello";
 
 			break;
 
 		}
 			
-		//Menyn
-		//Input
+		
 		//Feedback
-		std::cout << "Loop: " << counter << "\n";
-		counter++;
-		std::cin.get();
 
 	}
 }
